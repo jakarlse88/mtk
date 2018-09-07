8ff08c148bd4f030d15b48eb81887b877199c3ff
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-09-07 16:29:13 +0200

    Redesign footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,46 +1,29 @@
 import React, { Component } from 'react';
 
+import './Footer.css';
+
 import Card from '../common/Card';
 
 export default class Footer extends Component {
-  render() {
-    return (
-      <div className="container-fluid bg-dark">
-        <div className="container">
-          <div className="row bg-dark text-light">
-            <div className="col-12 col-sm-6 col-lg-4">
-              <address className="mt-3">
-                <p className="lead">Kontakt oss</p>
-                <b>Moss Taekwondo Klubb</b>
-                <br />
-                Rabekkgata 5 <br />
-                N-1523, Moss <br />
-                <br />
-                Leder: leder (@) moss-tkd.no <br />
-                Kasserer: kasserer (@) moss-tkd.no
-              </address>
-            </div>
-            <div className="col-12 col-sm-6 col-lg-4">
-              <Card
-                darkStyle={true}
-                cardTitle="Begynne å trene?"
-                cardText="Vi har løpende inntak av nye medlemmer, og to ukers uforpliktende prøvetid. Se våre treningstider, eller kontakt oss om du har spørsmål."
-              />
-            </div>
-            <div className="col-12 col-sm-6 col-lg-4">
-              <p className="lead mt-3">Følg oss på sosiale medier!</p>
-            </div>
-            <div className="col-12">
-              <p className="text-center">
-                <small>
-                  Copyright &copy; {new Date().getFullYear()} Moss
-                  Taekwondo Klubb
-                </small>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<footer className="footer">
+				<div className="container text-center">
+					<small className="text-muted">
+						Copyright &copy; {new Date().getFullYear()} Moss
+						Taekwondo Klubb
+					</small>
+					<br />
+					<small className="text-muted">
+						Made with &hearts; by{' '}
+						<a
+							href="https://github.com/jakarlse88"
+							target="_blank">
+							Jon Karlsen
+						</a>
+					</small>
+				</div>
+			</footer>
+		);
+	}
 }
