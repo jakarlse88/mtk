a7a9dcb8a3b6970d95eaeda3407f7b5d89cd5220
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-09-07 21:38:15 +0200

    Prototype Dashboard layout

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,9 +3,41 @@ import React, { Component } from 'react';
 export default class Dashboard extends Component {
 	render() {
 		return (
-			<div>
-				<h2>Dashboard</h2>
+			<div className="container">
+				<h2 className="h3 text-center mt-4">Dashboard</h2>
+				<div className="row mt-4 mb-4">
+					{items.map(item => (
+						<div className="col-12 col-sm-6 col-lg-4 m-auto">
+							<div className="card mb-4">
+								<img
+									src="https://via.placeholder.com/350x185"
+									alt="Descriptive image"
+									className="card-img-top"
+								/>
+								<div className="card-body">
+									<h5 className="card-title">
+										Something Something
+									</h5>
+									<p className="card-text">
+										Lorem ipsum dolor sit amet,
+										consectetur adipisicing elit.
+										Cupiditate, dolor! Maxime ex
+										nostrum quae explicabo vel
+										voluptatum soluta quisquam optio!
+									</p>
+								</div>
+								<div className="card-footer">
+									<small className="text-muted">
+										Permissions here
+									</small>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
 		);
 	}
 }
+
+const items = ['', '', '', '', '', ''];
