7eaa26b20147bd4046e87add3915521ebd09a0ba
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-09-07 14:18:29 +0200

    Implement Login component + functionality

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,71 +1,129 @@
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-import { loginUser, setCurrentUser } from '../../actions/authActions';
+import { loginUser } from '../../actions/authActions';
 
-export default class Login extends Component {
-  constructor(props) {
-    super(props);
+class Login extends Component {
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      username: '',
-      password: ''
-    };
-  }
+		this.state = {
+			email: '',
+			password: '',
+			errors: {}
+		};
+	}
 
-  onChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
+	componentDidMount = () => {
+		if (this.props.auth.isAuthenticated) {
+			this.props.history.push('/');
+			// TODO: dashboard
+		}
+	};
 
-  onSubmit = () => {
-    const loginData = {
-      username: this.state.username,
-      password: this.state.password
-    };
+	componentWillReceiveProps = nextProps => {
+		if (nextProps.auth.isAuthenticated) {
+			this.props.history.push('/');
+			// TODO: dashboard
+		}
 
-    console.log(loginData);
-  };
+		if (nextProps.errors) {
+			this.setState({
+				errors: nextProps.errors
+			});
+		}
+	};
 
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-8 m-auto">
-            <h4 className="text-center mt-4">Admin/instruktør login</h4>
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <input
-                  value={this.state.username}
-                  onChange={this.onChange}
-                  id="loginNameInput"
-                  type="text"
-                  name="username"
-                  placeholder="Brukernavn"
-                  className="form-control"
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  value={this.state.password}
-                  onChange={this.onChange}
-                  name="password"
-                  id="loginPasswordInput"
-                  type="password"
-                  placeholder="Passord"
-                  className="form-control"
-                />
-              </div>
-            </form>
-            <button
-              className="btn btn-dark mb-4"
-              type="submit"
-              onClick={this.onSubmit}>
-              Login
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	onChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+
+	onSubmit = () => {
+		const loginData = {
+			email: this.state.email,
+			password: this.state.password
+		};
+
+		this.props.loginUser(loginData);
+	};
+
+	render() {
+		const { errors } = this.state;
+
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-8 m-auto">
+						<h4 className="text-center mt-4">
+							Admin/instruktør login
+						</h4>
+						<form onSubmit={this.onSubmit}>
+							<div className="form-group">
+								<input
+									value={this.state.email}
+									onChange={this.onChange}
+									id="loginNameInput"
+									type="text"
+									name="email"
+									placeholder="Brukernavn"
+									className="form-control"
+								/>
+								{errors.email ? (
+									<small
+										id="emailHelp"
+										className="form-text text-danger">
+										{errors.email}
+									</small>
+								) : null}
+							</div>
+							<div className="form-group">
+								<input
+									value={this.state.password}
+									onChange={this.onChange}
+									name="password"
+									id="loginPasswordInput"
+									type="password"
+									placeholder="Passord"
+									className="form-control"
+								/>
+								{errors.password ? (
+									<small
+										id="passwordHelp"
+										className="form-text text-danger">
+										{errors.password}
+									</small>
+								) : null}
+							</div>
+						</form>
+						<button
+							className="btn btn-dark mb-4"
+							type="submit"
+							onClick={this.onSubmit}>
+							Login
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
+
+Login.propTypes = {
+	auth: PropTypes.object.isRequired,
+	errors: PropTypes.object.isRequired,
+	loginUser: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+	auth: state.auth,
+	errors: state.errors
+});
+
+export default connect(
+	mapStateToProps,
+	{ loginUser }
+)(withRouter(Login));
