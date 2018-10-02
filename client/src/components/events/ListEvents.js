672ba98b48f0d90d293624a0ecfdbd1e364393f6
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-10-02 12:43:08 +0200

    Pre-emptive safety commit

--- client/src/components/events/ListEvents.js
+++ client/src/components/events/ListEvents.js
@@ -6,13 +6,17 @@ import React, { Component } from 'react';
 
 import { getEventsArr } from '../../actions/eventActions';
 
+// TODO: pagination
+
 class ListEvents extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			events: {},
-			errors: {}
+			errors: {},
+			currentPage: 1,
+			eventsPerPage: 5
 		};
 	}
 
@@ -34,31 +38,101 @@ class ListEvents extends Component {
 		}
 	};
 
+	onClick = e => {
+		this.setState({
+			currentPage: Number(e.target.id)
+		});
+	};
+
 	render() {
 		const { auth } = this.props;
-		const { events, errors } = this.state;
+		const { currentPage, eventsPerPage, events, errors } = this.state;
 
 		let content;
 
+		// Event display logic
+		const indexOfLastEvent = currentPage * eventsPerPage;
+		const indexOfFirstEvent = indexOfLastEvent - eventsPerPage;
+		const currentEvents = events.eventsArr.slice(
+			indexOfFirstEvent,
+			indexOfLastEvent
+		);
+
+		const renderEvents = currentEvents.map((event, index) => (
+			<div className="col s12 l6">
+				<div className="card horizontal hoverable" key={index}>
+					<div className="card-stacked">
+						<div className="card-content">
+							<span className="card-title">{event.name}</span>
+							<small className="grey-text">
+								{event.eventType},{' '}
+								<Moment date={event.startDate} format="dddd DD/MM/YYYY" />{' '}
+								to <Moment date={event.endDate} format="dddd DD/MM/YYYY" />{' '}
+								by {event.owner}
+							</small>
+							<br />
+							<br />
+							<p>{event.description}</p>
+						</div>
+						<div className="card-action">
+							<Link to={`/single-event/${event._id}`}>
+								<button className="btn blue waves-effect waves-blue">
+									<i className="fas fa-search fa-xl left" />
+									View event
+								</button>
+							</Link>
+							{auth.user.role === 'admin' && (
+								<Link
+									to={`/admin-event/${event._id}`}
+									className="red-text">
+									<button className="btn blue waves-effect waves-blue">
+										<i className="fas fa-cogs fa-xl left" />
+										Administrate event
+									</button>
+								</Link>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		));
+
+		// Logic for displaying page numbers
+		const pageNumbers = [];
+		for (
+			let i = 1;
+			i <= Math.ceil(events.eventsArr.length / eventsPerPage);
+			i++
+		) {
+			pageNumbers.push(i);
+		}
+
+		const renderPageNumbers = pageNumbers.map(number => (
+			<li
+				className="waves-effect"
+				id={number}
+				key={number}
+				onClick={this.onClick}>
+				{number}
+			</li>
+		));
+
 		if (events.eventLoading) {
 			content = (
-				<p className="lead text-center">
-					<span className="badge">
-						<i className="fa fa-spinner fa-spin fa-3x" />
-					</span>
+				<p className="flow-text center-align">
+					<i className="fa fa-spinner fa-spin fa-3x" />
 				</p>
 			);
 		}
 
 		if (Object.keys(errors).length > 0) {
 			content = (
-				<p className="lead text-center text-danger">
-					<span className="badge">
-						<i className="fas fa-exclamation-triangle fa-3x" /> <br />
-						<br />
-						<br />
-						Oops! Something went wrong. Try again, or contact an admin.
-					</span>
+				<p className="flow-text center-align red-text">
+					<i className="fas fa-exclamation-triangle fa-3x" />
+					<br />
+					<br />
+					<br />
+					Oops! Something went wrong. Try again, or contact an admin.
 				</p>
 			);
 		}
@@ -68,38 +142,44 @@ class ListEvents extends Component {
 			events.eventsArr instanceof Array
 		) {
 			content = events.eventsArr.map((event, index) => (
-				<div key={index} className="card mb-2">
-					<div className="card-body">
-						<h5 className="card-title">{event.name}</h5>
-						<p className="card-text">{event.description}</p>
-						<Link to={`/single-event/${event._id}`}>
-							<span className="badge">
-								<i className="fas fa-search fa-xl mt-2" />
-							</span>{' '}
-							View event
-						</Link>
-						<br />
-						{auth.user.role === 'admin' && (
-							<Link
-								to={`/admin-event/${event._id}`}
-								className="text-warning">
-								<span className="badge">
-									<i className="fas fa-cogs fa-xl mt-2" />
-								</span>{' '}
-								Administrate event
-							</Link>
-						)}
-					</div>
-					<div className="card-footer">
-						<span className="badge text-muted text-">
-							<i className="fas fa-info-circle fa-lg" />
-						</span>
-						<small className="text-muted">
-							{event.eventType},{' '}
-							<Moment date={event.startDate} format="dddd DD/MM/YYYY" /> to{' '}
-							<Moment date={event.endDate} format="dddd DD/MM/YYYY" /> by{' '}
-							{event.owner}
-						</small>
+				<div className="col s12 l6">
+					<div className="card horizontal hoverable" key={index}>
+						<div className="card-stacked">
+							<div className="card-content">
+								<span className="card-title">{event.name}</span>
+								<small className="grey-text">
+									{event.eventType},{' '}
+									<Moment
+										date={event.startDate}
+										format="dddd DD/MM/YYYY"
+									/>{' '}
+									to{' '}
+									<Moment date={event.endDate} format="dddd DD/MM/YYYY" />{' '}
+									by {event.owner}
+								</small>
+								<br />
+								<br />
+								<p>{event.description}</p>
+							</div>
+							<div className="card-action">
+								<Link to={`/single-event/${event._id}`}>
+									<button className="btn blue waves-effect waves-blue">
+										<i className="fas fa-search fa-xl left" />
+										View event
+									</button>
+								</Link>
+								{auth.user.role === 'admin' && (
+									<Link
+										to={`/admin-event/${event._id}`}
+										className="red-text">
+										<button className="btn blue waves-effect waves-blue">
+											<i className="fas fa-cogs fa-xl left" />
+											Administrate event
+										</button>
+									</Link>
+								)}
+							</div>
+						</div>
 					</div>
 				</div>
 			));
@@ -108,29 +188,31 @@ class ListEvents extends Component {
 		return (
 			<div className="container">
 				<div className="row">
-					<div className="col-12 m-auto">
-						<h2 className="display-4 text-center mt-4">Events</h2>
-						<hr />
+					<div className="col s12">
+						<h2 className="center-align">Events</h2>
+						<div className="divider" />
 					</div>
-					<div className="col-10 m-auto">{content}</div>
+					<div className="row">
+						<div className="col" />
+					</div>
+					<div className="col s12">{content}</div>
+					<div className="row">
+						<div className="col" />
+					</div>
 					{this.props.auth.isAuthenticated ? (
-						<div className="col-8 m-auto text-center">
+						<div className="col s12 center-align">
 							<Link to="/manage-events">
 								<button className="btn btn-secondary mt-2 mb-4">
-									<span className="badge">
-										<i className="fas fa-arrow-left" />
-									</span>{' '}
+									<i className="fas fa-arrow-left left" />
 									Back
 								</button>
 							</Link>
 						</div>
 					) : (
-						<div className="col-8 m-auto text-center">
+						<div className="col s12 center-align">
 							<Link to="/">
-								<button className="btn btn-secondary mt-2 mb-4">
-									<span className="badge">
-										<i className="fas fa-arrow-left" />
-									</span>{' '}
+								<button className="btn grey">
+									<i className="fas fa-arrow-left" />
 									Home
 								</button>
 							</Link>
